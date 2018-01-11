ba4f1712ebb9802df1afd31c66ee550f9689c391
guiltylotus <minh02091997@gmail.com>
2018-01-11 21:43:28 +0700

    first

---
+++ lk_track.py
@@ -0,0 +1,76 @@
+import numpy as np
+import cv2
+import sys
+
+
+def detectAndDescribe(image):
+    # convert the image to grayscale
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # detect and extract features from the image
+    descriptor = cv2.xfeatures2d.SIFT_create()
+    (kps, features) = descriptor.detectAndCompute(image, None)
+
+    # convert the keypoints from KeyPoint objects to NumPy
+    # arrays
+    kps = [kp.pt for kp in kps]
+    # return a tuple of keypoints and features
+    return np.expand_dims(np.asarray(kps, dtype=np.float32), axis= 1)
+
+cap = cv2.VideoCapture(sys.argv[1])
+
+# params for ShiTomasi corner detection
+feature_params = dict(maxCorners=100,
+                      qualityLevel=0.3,
+                      minDistance=7,
+                      blockSize=7)
+# Parameters for lucas kanade optical flow
+lk_params = dict(winSize=(15, 15),
+                 maxLevel=2,
+                 criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
+# Create some random colors
+color = np.random.randint(0, 255, (1000, 3))
+# Take first frame and find corners in it
+ret, old_frame = cap.read()
+old_gray = cv2.cvtColor(old_frame, cv2.COLOR_BGR2GRAY)
+# p0 = cv2.goodFeaturesToTrack(old_gray, mask=None, **feature_params)
+# print p0.dtype
+
+p0 = detectAndDescribe(old_frame)
+print p0.dtype
+# Create a mask image for drawing purposes
+mask = np.zeros_like(old_frame)
+idx = 0
+while(1):
+    idx += 1
+    ret, frame = cap.read()
+    if ret:
+        frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        if idx % 30 == 0:
+            p0 = detectAndDescribe(frame)
+        # calculate optical flow
+        p1, st, err = cv2.calcOpticalFlowPyrLK(
+            old_gray, frame_gray, p0, None, **lk_params)
+        # Select good points
+        good_new = p1[st == 1]
+        good_old = p0[st == 1]
+        # draw the tracks
+        for i, (new, old) in enumerate(zip(good_new, good_old)):
+            a, b = new.ravel()
+            c, d = old.ravel()
+            # mask = cv2.line(mask, (a, b), (c, d), color[i].tolist(), 2)
+            frame = cv2.circle(frame, (a, b), 2, color[i].tolist(), -1)
+        img = cv2.add(frame, mask)
+        cv2.imshow('frame', img)
+        k = cv2.waitKey(30) & 0xff
+        if k == 27:
+            break
+        if k == ord('a'):
+            cv2.waitKey(0)
+        # Now update the previous frame and previous points
+        old_gray = frame_gray.copy()
+        p0 = good_new.reshape(-1, 1, 2)
+    else:
+        break
+cv2.destroyAllWindows()
+cap.release()
