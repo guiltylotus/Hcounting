b3ce5b69664a6a553a26ec3f1740f4859a881320
guiltylotus <minh02091997@gmail.com>
2018-01-16 23:03:48 +0700

    contour + bb

--- BG_subtract.py
+++ BG_subtract.py
@@ -1,9 +1,9 @@
 import numpy as np
 import cv2
-from sample import *
+from myclass import *
 
-_sam = Sample()
-cap = cv2.VideoCapture('video/video1.mp4')
+_sam = myClass()
+cap = cv2.VideoCapture('video/video4-4.mp4')
 fgbg = cv2.createBackgroundSubtractorMOG2(detectShadows = True)
 
 count = 0
@@ -14,18 +14,10 @@ while(1):
     #     break
     ret, frame = cap.read()
     fgmask = fgbg.apply(frame)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     ret, fgmask = cv2.threshold(fgmask,127,255,cv2.THRESH_BINARY)  #remove shadow
-    fgmask, contours, hierarchy = cv2.findContours(fgmask,1,2)
+    bb_img = _sam.exContours(fgmask)
     # print(len(contours))
 
-    if (len(contours) != 0):
-        cnt = contours[0]
-        # print(cnt)
-        rect = cv2.minAreaRect(cnt)
-        box = cv2.boxPoints(rect)
-        box = np.int0(box)
-        cv2.drawContours(fgmask,[box],0,(0,0,255),2)
     
     # fgmask = _sam.takeSample(fgmask, 100)
     
---
+++ myclass.py
@@ -0,0 +1,44 @@
+import numpy as np 
+import cv2
+
+class myClass():
+
+    #sampling
+    def takeSample(self,frame, rate):
+        h,w = np.shape(frame)
+
+        for i in range(h):
+            for j in range(w):
+                if (frame[i,j] != 0):
+                    r = np.random.randint(100)
+                    # print(r)
+                    if (r >= rate):
+                        frame[i,j] = 0
+                
+        return frame
+
+    #extract contours
+    def exContours(self,img):
+        #input : frame 
+        
+        img, contours, hierarchy = cv2.findContours(img,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+
+        # print(len(contours))
+        # print(contours)
+
+        # cv2.drawContours(img, contours, -1, (128,255,0), 3)
+        if (len(contours) != 0):
+            # print(len(contours))
+            for cnt in contours:
+                #rotate
+                # print(cnt)
+                # rect = cv2.minAreaRect(cnt)
+                # box = cv2.boxPoints(rect)
+                # box = np.int0(box)
+                # cv2.drawContours(img,[box],0,(128,255,0),2)
+                
+                #rectangle
+                x,y,w,h = cv2.boundingRect(cnt)
+                cv2.rectangle(img,(x,y),(x+w,y+h),(128,255,0),2)
+        #output: frame + bb
+
